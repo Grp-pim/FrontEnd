7296ba2747da9ca1d0bdca30f36e72057abd9c85
ZAGHDOUDI Yesser <126217823+yesserZgh@users.noreply.github.com>
2024-05-01 19:05:11 +0200

    stepper

--- src/app/shared/shared.service.ts
+++ src/app/shared/shared.service.ts
@@ -4,17 +4,12 @@ import { Observable, BehaviorSubject, Subject } from 'rxjs';
   providedIn: 'root',
 })
 export class SharedService {
-  private currentChapterSubject = new BehaviorSubject<number>(1);
-  currentChapter$ = this.currentChapterSubject.asObservable();
+
 
   private startGameSubject = new Subject<boolean>();
 
   constructor() {}
 
-
-  setCurrentChapter(chapter: number) {
-    this.currentChapterSubject.next(chapter);
-  }
 
   triggerStartGame(autoPlay: boolean): void {
     this.startGameSubject.next(autoPlay);
@@ -23,20 +18,8 @@ export class SharedService {
   getStartGameObservable(): Observable<boolean> {
     return this.startGameSubject.asObservable();
   }
+  
 
-  fetchNextChapterTask(): void {
-    // Émettre le prochain numéro de chapitre
-    const currentChapter = this.currentChapterSubject.value;
-    this.currentChapterSubject.next(currentChapter + 1);
-  }
-
-  fetchPreviousChapterTask(): void {
-    // Émettre le numéro de chapitre précédent si possible
-    const currentChapter = this.currentChapterSubject.value;
-    if (currentChapter > 1) {
-      this.currentChapterSubject.next(currentChapter - 1);
-    }
-  }
 }
 
 
